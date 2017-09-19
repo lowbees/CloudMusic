711eacd62e2a8160e93b24eaf36cde2b2e61e26c
lowbees <demoresh@126.com>
2017-09-19 16:51:15 +0800

    Update framelesswindowhelper.cpp

--- CloudMusicPlayer/FramelessHelper/framelesswindowhelper.cpp
+++ CloudMusicPlayer/FramelessHelper/framelesswindowhelper.cpp
@@ -41,47 +41,16 @@ void FramelessWindowHelper::componentComplete()
     }
 }
 
-void FramelessWindowHelper::addWindow(const QString &objName)
-{
-    //    if (!engine || engine->rootObjects().isEmpty() || objName.isEmpty())
-    //        return;
-
-    //    for (int i = 0; i < engine->rootObjects().size(); ++i) {
-    //        QObject *rootObj = engine->rootObjects()[i];
-
-    //        if (rootObj) {
-    //            if (rootObj->isWindowType() && !windows.contains(rootObj)) {
-    //                windows.insert(rootObj, new WindowHandler(qobject_cast<QQuickWindow*>(rootObj)));
-    //                rootObj->installEventFilter(this);
-    //            }
-
-    //            QObjectList objs = rootObj->findChildren<QObject*>(objName);
-    //            for (int j = 0; j < objs.size(); ++j) {
-    //                if (objs[j]->isWindowType() && !windows.contains(objs[j])) {
-    //                    windows.insert(objs[j], new WindowHandler(qobject_cast<QQuickWindow*>(objs[j])));
-    //                    objs[j]->installEventFilter(this);
-    //                }
-    //            }
-    //        }
-    //    }
-}
-
-void FramelessWindowHelper::addWindow(QObject *obj)
-{
-    if (obj) addWindow(obj->objectName());
-}
 
 
 bool FramelessWindowHelper::eventFilter(QObject *watched, QEvent *event)
 {
-//    qDebug() << watched << "eventFilter";
     switch (event->type()) {
     case QEvent::MouseButtonPress:
     case QEvent::MouseMove:
     case QEvent::MouseButtonRelease:
     {
         WindowHandler *handler = windows.value(watched);
-        qDebug() << handler << "handler";
         if (handler) {
             handler->handleEvent(event);
             if (handler->isResizing())
